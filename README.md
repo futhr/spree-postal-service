367876f5aad93cbd3b66919163c2d171093a012e
Torsten Rüger <torsten@lightning.nu>
2011-01-04 13:40:14 +0200

    cleans up code, better readme, locale

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 SpreePostalService
 ==================
 
-A Postal service is delivers based on weight only(*). Like most post services in europe will.
+A postal service is delivers based on weight only(*). Like most post services in europe will.
 
 This spree extension adds a spree-calculator to model this.
 
@@ -11,10 +11,12 @@ Other features:
   - handling fee may be added ( with a maximum when it won't be applied anymore)
   - multi-parcel shipments are automatically created
 
-Off course this relies on your weight data to be correct (and if you want the restrictions to work, the size data too)
+Off course this relies on your weight data to be correct (and if you want the restrictions to work, the size data too).
+Use the same measurements as in the product info page.
 
+(*) you may instal several ShippingMethods for (usually) different countries.
 
-Example
+Usage
 =======
 
 Add to your gemfile
@@ -29,6 +31,41 @@ and use "Postal"
 
 as calculator.
 
+The size/weight "table" must have the same amount of (space separated) entries.
+
 Optionally add your own locale to config/locales/  (and if you do, send it to me)
+
+Example:
+=======
+
+With the default settings (measurements in kg and cm):
+
+- Max weight of one item: 18
+- Max width of one item: 60
+- Max length of one item: 90
+- Default weight: 1kg  (applies when product weight is 0)
+- Handling fee: 10
+- Amount, over which handling fee won't be applied: 50
+- Minimum total of the order: 10.0
+- Weights (space seperated): 1 2 5 10 20
+- Prices (space seperated):  6 9 12 15 18
+
+Applies?
+-------
+The Shipping method does not apply to the order if:
+
+- The order total is under 10 Euroes
+- Any items weighs more than 18 Kg
+- Any item is longer than 90 cm
+- Any items second longest side (width) is over 60cm. Eg a 70x70x20 item.
+ 
+Costs
+-----
+- items weighing 10 kg of worth 100 Euros will cost 15 Euros
+- items weighing 10 kg of worth 40 Euros will cost 25 Euros (15 + 10 handling)
+- items weighing less than 1 kg of worth 60 Euros will cost 6 Euros 
+- items weighing less than 1 kg of worth 40 Euros will cost 16 Euros (6 + 10) 
+- items weighing 25 kg of worth 200 Euros will cost 30 Euros (2 packages, 18 + 12 euro)
+- 3 items without weight inforamtion of worth 100 euros will cost 12 Euro
 
 Copyright (c) 2011 [name of extension creator], released under the New BSD License
--- lib/calculator/postal_service.rb
+++ lib/calculator/postal_service.rb
@@ -1,4 +1,3 @@
-require 'shipping_method'
 
 class Calculator::PostalService < Calculator
   preference :weight_table, :string, :default => "1 2 5 10 20"
@@ -9,6 +8,7 @@ class Calculator::PostalService < Calculator
   preference :min_price, :float, :default => 10
   preference :handling_max, :float, :default => 50
   preference :handling_fee, :float, :default => 10
+  preference :default_weight, :float, :default => 1
   
   def self.description
     "Postal"
@@ -29,34 +29,33 @@ class Calculator::PostalService < Calculator
     return false
   end
   
+  def available?(order)
+    return false if order.total < self.preferred_min_price
+    order.line_items.each do |item| # determine if weight or size goes over bounds
+      return false if item.variant.weight and item.variant.weight > self.preferred_max_item_weight
+      return false if item_oversized?( item )
+    end
+    return true
+  end
+  
   # as order_or_line_items we always get line items, as calculable we have Coupon, ShippingMethod or ShippingRate
   def compute(order)
     debug = false
     puts order if debug
-    line_items = order.line_items
-
-    pain = 1
     
     total_price , total_weight , shipping  = 0 , 0 , 0 
     prices = self.preferred_price_table.split.map {|price| price.to_f }
     puts prices.join(" ")  if debug
-    pain = 0 
     
-    line_items.each do |item| # determine total price and weight
-      total_weight += item.variant.weight ? item.variant.weight * item.quantity : 3 * item.quantity
+    order.line_items.each do |item| # determine total price and weight
+      total_weight += item.quantity * (item.variant.weight  || self.preferred_default_weight)
       total_price += item.price * item.quantity
-      if item.variant.weight and item.variant.weight > self.preferred_max_item_weight
-          pain +=  prices.last * 2 * self.preferred_max_item_weight / item.variant.weight  
-      end
-      if item_oversized?( item )
-        pain +=  prices.last * 2 * self.preferred_max_item_weight / item.variant.weight  
-      end
     end
     puts "Weight " + total_weight.to_s  if debug
     puts "Price " + total_price.to_s if debug
     # determine handling fee
-    handling_fee = self.preferred_handling_max < total_price  ? 0 : self.preferred_handling_fee
     puts "Handling max  " + self.preferred_handling_max.to_s  if debug
+    handling_fee = self.preferred_handling_max < total_price  ? 0 : self.preferred_handling_fee
     puts "Handling  " + handling_fee.to_s  if debug
     weights = self.preferred_weight_table.split.map {|weight| weight.to_f} 
     puts weights.join(" ")  if debug
@@ -67,14 +66,12 @@ class Calculator::PostalService < Calculator
     puts "Shipping  " + shipping.to_s  if debug
     index = weights.length - 2
     while index >= 0
-      if weights[index] < total_weight
-        break
-      end
+      break if total_weight > weights[index] 
       index -= 1
     end
     shipping +=  prices[index + 1] 
     puts "Shipping  " + shipping.to_s  if debug
 
-    return shipping + handling_fee + pain
+    return shipping + handling_fee 
   end
 end
