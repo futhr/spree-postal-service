c6e14cd3aa46ade5b290e75160524098f4dae606
Filipe Dias <f.dias@karma-consulting.pt>
2012-11-23 15:52:53 +0000

    Added zip code and max total weight verification preferences.

--- lib/spree/calculator/postal_service.rb
+++ lib/spree/calculator/postal_service.rb
@@ -5,12 +5,15 @@ class Spree::Calculator::PostalService < Spree::Calculator
   preference :max_item_weight, :decimal, :default => 18
   preference :max_item_width, :decimal, :default => 60
   preference :max_item_length, :decimal, :default => 120
+  preference :max_total_weight, :decimal, :default => 0
   preference :max_price, :decimal, :default => 120
   preference :handling_max, :decimal, :default => 50
   preference :handling_fee, :decimal, :default => 10
   preference :default_weight, :decimal, :default => 1
+  preference :zipcode_handling, :string, :default => nil, :description => '<empty> does not apply, exact, starts, ends, contains'
+  preference :zipcode, :string, :default => nil
 
-  attr_accessible :preferred_weight_table, :preferred_price_table, :preferred_max_item_weight, :preferred_max_item_width, :preferred_max_item_length, :preferred_max_price, :preferred_handling_max, :preferred_handling_fee, :preferred_default_weight
+  attr_accessible :preferred_weight_table, :preferred_price_table, :preferred_max_item_weight, :preferred_max_item_width, :preferred_max_item_length, :preferred_max_price, :preferred_handling_max, :preferred_handling_fee, :preferred_default_weight, :preferred_zipcode_handling, :preferred_zipcode, :preferred_max_total_weight
   
   def self.description
     "Postal"
@@ -22,6 +25,33 @@ class Spree::Calculator::PostalService < Spree::Calculator
 #    ShippingMethod.register_calculator(self)
   end
   
+  def order_total_weight(order)
+    total_weight = 0
+    order.line_items.each do |item| # determine total price and weight
+      total_weight += item.quantity * (item.variant.weight  || self.preferred_default_weight)
+    end
+    return total_weight
+  end
+
+  def handle_zipcode?(order)
+    return true if self.preferred_zipcode_handling.blank?
+    
+    if(self.preferred_zipcode_handling == 'exact')
+      return self.preferred_zipcode.downcase == order.ship_address.zipcode.downcase 
+    end
+    if(self.preferred_zipcode_handling == 'starts')
+      return order.ship_address.zipcode.downcase.start_with?(self.preferred_zipcode.downcase)
+    end
+    if(self.preferred_zipcode_handling == 'ends')
+      return order.ship_address.zipcode.downcase.end_with?(self.preferred_zipcode.downcase)
+    end
+    if(self.preferred_zipcode_handling == 'contains')
+      return order.ship_address.zipcode.downcase.include?(self.preferred_zipcode.downcase) 
+    end
+
+    return true
+  end
+
   def item_oversized? item
     variant = item.variant
     sizes = [ variant.width ? variant.width : 0 , variant.depth ? variant.depth : 0 , variant.height ? variant.height : 0 ].sort!
@@ -30,12 +60,19 @@ class Spree::Calculator::PostalService < Spree::Calculator
     return true if sizes[0] > self.preferred_max_item_width
     return false
   end
+
+  def total_overweight? order
+    return false if self.preferred_max_total_weight == 0
+    return order_total_weight(order) > self.preferred_max_total_weight
+  end
   
   def available?(order)
+    return false if !handle_zipcode?(order)
     order.line_items.each do |item| # determine if weight or size goes over bounds
       return false if item.variant.weight and item.variant.weight > self.preferred_max_item_weight
       return false if item_oversized?( item )
     end
+    return false if total_overweight?(order)
     return true
   end
   
