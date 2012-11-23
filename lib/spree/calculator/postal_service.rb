8cb79621a3ac83e1e0cd5b4c64f2c453d2f6fc26
Filipe Dias <f.dias@karma-consulting.pt>
2012-11-23 19:08:34 +0000

    Added flags to enable/disable verifications and features instead of using the 0 value. Added support to add several zip codes to be verified.

--- lib/spree/calculator/postal_service.rb
+++ lib/spree/calculator/postal_service.rb
@@ -2,18 +2,30 @@
 class Spree::Calculator::PostalService < Spree::Calculator
   preference :weight_table, :string, :default => "1 2 5 10 20"
   preference :price_table, :string, :default => "6 9 12 15 18"
+  preference :max_item_weight_enabled, :boolean, :default => true
   preference :max_item_weight, :decimal, :default => 18
+  preference :max_item_width_enabled, :boolean, :default => true
   preference :max_item_width, :decimal, :default => 60
+  preference :max_item_length_enabled, :boolean, :default => true
   preference :max_item_length, :decimal, :default => 120
+  preference :max_total_weight_enabled, :boolean, :default => false
   preference :max_total_weight, :decimal, :default => 0
+  preference :max_price_enabled, :boolean, :default => false
   preference :max_price, :decimal, :default => 120
   preference :handling_max, :decimal, :default => 50
   preference :handling_fee, :decimal, :default => 10
   preference :default_weight, :decimal, :default => 1
   preference :zipcode_handling, :string, :default => nil
-  preference :zipcode, :string, :default => nil
+  preference :zipcode_separator, :string, :default => '|'
+  preference :zipcodes, :string, :default => nil
 
-  attr_accessible :preferred_weight_table, :preferred_price_table, :preferred_max_item_weight, :preferred_max_item_width, :preferred_max_item_length, :preferred_max_price, :preferred_handling_max, :preferred_handling_fee, :preferred_default_weight, :preferred_zipcode_handling, :preferred_zipcode, :preferred_max_total_weight
+  attr_accessible :preferred_weight_table, :preferred_price_table, :preferred_max_item_weight,
+    :preferred_max_item_width, :preferred_max_item_length, :preferred_max_price,
+    :preferred_handling_max, :preferred_handling_fee, :preferred_default_weight,
+    :preferred_zipcode_handling, :preferred_zipcode_separator, :preferred_zipcodes,
+    :preferred_max_total_weight, :preferred_max_item_weight_enabled,
+    :preferred_max_item_width_enabled, :preferred_max_item_length_enabled,
+    :preferred_max_total_weight_enabled, :preferred_max_price_enabled 
   
   def self.description
     "Postal"
@@ -33,44 +45,53 @@ class Spree::Calculator::PostalService < Spree::Calculator
     return total_weight
   end
 
+  def zipcodes
+    return @zipcodes if @zipcodes
+    return [""] if self.preferred_zipcodes.blank? || self.preferred_zipcode_separator.blank?
+
+     self.preferred_zipcodes.downcase.split(self.preferred_zipcode_separator)
+  end
+
   def handle_zipcode?(order)
     return true if self.preferred_zipcode_handling.blank?
-    
-    if(self.preferred_zipcode_handling == 'exact')
-      return self.preferred_zipcode.downcase == order.ship_address.zipcode.downcase 
+    result = false
+    zipcodes.each do |zipcode|
+      if(self.preferred_zipcode_handling == 'exact')
+        result = zipcode == order.ship_address.zipcode.downcase
+      end
+      if(self.preferred_zipcode_handling == 'starts')
+        result = order.ship_address.zipcode.downcase.start_with?(zipcode)
+      end
+      if(self.preferred_zipcode_handling == 'ends')
+        result = order.ship_address.zipcode.downcase.end_with?(zipcode)
+      end
+      if(self.preferred_zipcode_handling == 'contains')
+        result = order.ship_address.zipcode.downcase.include?(zipcode) 
+      end
+      break if result
     end
-    if(self.preferred_zipcode_handling == 'starts')
-      return order.ship_address.zipcode.downcase.start_with?(self.preferred_zipcode.downcase)
-    end
-    if(self.preferred_zipcode_handling == 'ends')
-      return order.ship_address.zipcode.downcase.end_with?(self.preferred_zipcode.downcase)
-    end
-    if(self.preferred_zipcode_handling == 'contains')
-      return order.ship_address.zipcode.downcase.include?(self.preferred_zipcode.downcase) 
-    end
-
-    return true
+    return result
   end
 
   def item_oversized? item
-    return false if self.preferred_max_item_length == 0 && self.preferred_max_item_width == 0
+    return false if self.preferred_max_item_length_enabled && self.preferred_max_item_width_enabled == 0
     variant = item.variant
     sizes = [ variant.width ? variant.width : 0 , variant.depth ? variant.depth : 0 , variant.height ? variant.height : 0 ].sort!
     #puts "Sizes " + sizes.join(" ")
-    return true if self.preferred_max_item_length > 0 && sizes[0] > self.preferred_max_item_length
-    return true if self.preferred_max_item_width > 0 && sizes[0] > self.preferred_max_item_width
+    return true if self.preferred_max_item_length_enabled && sizes[0] > self.preferred_max_item_length
+    return true if self.preferred_max_item_width_enabled && sizes[0] > self.preferred_max_item_width
     return false
   end
 
   def total_overweight? order
-    return false if self.preferred_max_total_weight == 0
+    return false if !self.preferred_max_total_weight_enabled
     return order_total_weight(order) > self.preferred_max_total_weight
   end
   
   def available?(order)
     return false if !handle_zipcode?(order)
     order.line_items.each do |item| # determine if weight or size goes over bounds
-      return false if self.preferred_max_item_weight > 0 && item.variant.weight && item.variant.weight > self.preferred_max_item_weight
+      return false if self.preferred_max_item_weight_enabled && item.variant.weight && item.variant.weight > self.preferred_max_item_weight
       return false if item_oversized?( item )
     end
     return false if total_overweight?(order)
@@ -92,7 +113,7 @@ class Spree::Calculator::PostalService < Spree::Calculator
     end
     puts "Weight " + total_weight.to_s  if debug
     puts "Price " + total_price.to_s if debug
-    return 0.0 if total_price > self.preferred_max_price    
+    return 0.0 if self.preferred_max_price_enabled && total_price > self.preferred_max_price    
     # determine handling fee
     puts "Handling max  " + self.preferred_handling_max.to_s  if debug
     handling_fee = self.preferred_handling_max < total_price  ? 0 : self.preferred_handling_fee
