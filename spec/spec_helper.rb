6a5d6a80b4c8af89ce8f95455a0ece34d0c6d31b
Tobias Bohwalli <hi@futhr.io>
2014-03-31 08:45:05 +0200

    Add full test suite.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,27 @@
-ENV['RAILS_ENV'] ||= 'test'
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/lib/spree_postal_service/engine'
+  add_group 'Libraries', 'lib'
+end
 
-require File.expand_path('../../../config/environment', __FILE__)
+ENV['RAILS_ENV'] = 'test'
+
+require File.expand_path('../dummy/config/environment.rb',  __FILE__)
 
 require 'rspec/rails'
+require 'i18n-spec'
+require 'shoulda-matchers'
+require 'ffaker'
 
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
+
+require 'spree/testing_support/factories'
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
   config.mock_with :rspec
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 end
 
 @configuration ||= AppConfiguration.find_or_create_by_name('Default configuration')
