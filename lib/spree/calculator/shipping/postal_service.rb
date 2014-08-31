c7c3082622dffc6664dc217b05d795abc876d401
Tobias Bohwalli <hi@futhr.io>
2014-08-31 20:36:01 +0200

    Refactor calculator and its spec.

--- lib/spree/calculator/shipping/postal_service.rb
+++ lib/spree/calculator/shipping/postal_service.rb
@@ -1,87 +1,104 @@
-class Spree::Calculator::Shipping::PostalService < Spree::ShippingCalculator
-  preference :weight_table,    :string,  default: '1 2 5 10 20'
-  preference :price_table,     :string,  default: '6 9 12 15 18'
-  preference :max_item_weight, :decimal, default: 18
-  preference :max_item_width,  :decimal, default: 60
-  preference :max_item_length, :decimal, default: 120
-  preference :max_price,       :decimal, default: 120
-  preference :handling_max,    :decimal, default: 50
-  preference :handling_fee,    :decimal, default: 10
-  preference :default_weight,  :decimal, default: 1
+module Spree
+  class Calculator
+    module Shipping
+      class PostalService < Spree::ShippingCalculator
 
-  def self.description
-    Spree.t(:postal_service)
-  end
+        preference :weight_table,    :string,  default: '1 2 5 10 20'
+        preference :price_table,     :string,  default: '6 9 12 15 18'
+        preference :max_item_weight, :decimal, default: 18
+        preference :max_item_width,  :decimal, default: 60
+        preference :max_item_length, :decimal, default: 120
+        preference :max_price,       :decimal, default: 120
+        preference :handling_max,    :decimal, default: 50
+        preference :handling_fee,    :decimal, default: 10
+        preference :default_weight,  :decimal, default: 1
 
-  def self.register
-    super
-  end
+        class << self
+          def description
+            Spree.t(:postal_service)
+          end
 
-  def item_oversized?(variant)
-    sizes = [
-      variant.width  ? variant.width  : 0,
-      variant.depth  ? variant.depth  : 0,
-      variant.height ? variant.height : 0
-    ].sort.reverse
+          def register
+            super
+          end
+        end
 
-    return true if sizes[0] > self.preferred_max_item_length # longest side
-    return true if sizes[1] > self.preferred_max_item_width  # second longest side
-    return false
-  end
+        attr_accessor :line_items
 
-  def available?(package)
-    variants = package.contents.map(&:variant)
-    variants.each do |variant| # determine if weight or size goes over bounds
-      return false if item_within_bounds? variant.weight # 18
-      return false if item_oversized? variant
+        def item_oversized?(variant)
+          sizes = [
+            variant.width  ? variant.width  : 0,
+            variant.depth  ? variant.depth  : 0,
+            variant.height ? variant.height : 0
+          ].sort.reverse
+
+          return true if sizes[0] > preferred_max_item_length # Longest side.
+          return true if sizes[1] > preferred_max_item_width  # Second longest side.
+          false
+        end
+
+        # Determine if weight or size goes over bounds.
+        def available?(package)
+          package.order.variants.each do |variant|
+            return false if item_within_bounds?(variant.weight) # 18
+            return false if item_oversized?(variant)
+          end
+          true
+        end
+
+        # As order_or_line_items we always get line items, as calculable we have
+        # Coupon, ShippingMethod or ShippingRate.
+        def compute(package)
+          @line_items ||= package.order.line_items
+          total_price, total_weight, shipping = compute_total_price, compute_total_weight, 0
+
+          return 0.0 if total_price > preferred_max_price
+
+          while total_weight > weights.last # In several packages if need be.
+            total_weight -= weights.last
+            shipping += prices.last
+          end
+
+          [shipping, prices[compute_index(total_weight)], handling_fee(total_price)].compact.sum
+        end
+
+        private
+
+        def compute_total_weight
+          line_items.map do |item|
+            item.quantity * (item.variant.weight || preferred_default_weight)
+          end.reduce(:+)
+        end
+
+        def compute_total_price
+          line_items.map { |item| item.price * item.quantity }.reduce(:+)
+        end
+
+        def compute_index(total_weight)
+          index = weights.length - 2
+          while index >= 0
+            break if total_weight > weights[index]
+            index -= 1
+          end
+          index + 1
+        end
+
+        def item_within_bounds?(weight)
+          weight && weight > preferred_max_item_weight
+        end
+
+        def handling_fee(total_price)
+          preferred_handling_max < total_price ? 0 : preferred_handling_fee
+        end
+
+        def prices
+          @prices ||= preferred_price_table.split.map { |price| price.to_f }
+        end
+
+        def weights
+          @weights ||= preferred_weight_table.split.map { |weight| weight.to_f }
+        end
+      end
     end
-    return true
-  end
-
-  # As order_or_line_items we always get line items, as calculable we have
-  # Coupon, ShippingMethod or ShippingRate
-  def compute(package)
-    order = package.order
-
-    total_price, total_weight, shipping = 0, 0, 0
-
-    order.line_items.each do |item| # determine total price and weight
-      total_weight += item.quantity * (item.variant.weight || self.preferred_default_weight)
-      total_price  += item.price * item.quantity
-    end
-
-    return 0.0 if total_price > self.preferred_max_price
-
-    while total_weight > weights.last # in several packages if need be
-      total_weight -= weights.last
-      shipping += prices.last
-    end
-
-    index = weights.length - 2
-    while index >= 0
-      break if total_weight > weights[index]
-      index -= 1
-    end
-
-    shipping += prices[index + 1]
-    return shipping + handling_fee(total_price)
-  end
-
-  private
-
-  def item_within_bounds?(weight)
-    weight && weight > self.preferred_max_item_weight
-  end
-
-  def handling_fee(total_price)
-    self.preferred_handling_max < total_price ? 0 : self.preferred_handling_fee
-  end
-
-  def prices
-    self.preferred_price_table.split.map { |price| price.to_f }
-  end
-
-  def weights
-    self.preferred_weight_table.split.map { |weight| weight.to_f }
   end
 end
--- spec/lib/postal_service_spec.rb
+++ spec/lib/postal_service_spec.rb
@@ -1,129 +1,115 @@
-describe Spree::Calculator::Shipping::PostalService do
+RSpec.describe Spree::Calculator::Shipping::PostalService do
 
-  let(:postal_service_calculator) { described_class.new }
+  subject { described_class.new }
 
-  describe 'return description' do
-    context 'in english' do
-      before { I18n.locale = :en }
-      specify do
-        expect(postal_service_calculator.description).to eq 'Postal Service'
-      end
-    end
-
-    context 'in any language' do
-      before { I18n.locale = :pt }
-      specify do
-        expect(postal_service_calculator.description).to eq Spree.t(:postal_service)
+  context 'returns description' do
+    %w(en pt sv).each do |locale|
+      it "in supported language: #{locale}" do
+        I18n.with_locale(locale.to_sym) do
+          expect(described_class.description).to eq Spree.t(:postal_service)
+        end
       end
     end
   end
 
-  describe ':using the default weight-price table: [1 2 5 10 20] => [6 9 12 15 18]' do
-    context '.compute:' do
+  describe 'using the default weight-price table: [1 2 5 10 20] => [6 9 12 15 18]' do
+    context '.compute(package)' do
       it 'gives 15.0 when total price is 100 and weight is 10kg' do
         create_our_package(weight: 10.0, price: 100.0, quantity: 1)
-        result = postal_service_calculator.compute(@package)
+        result = subject.compute(@package)
         expect(result).to eq(15.0)
       end
 
       it 'gives 25.0 when total price is 40 and weight is 10kg' do
         create_our_package(weight: 10.0, price: 40.0, quantity: 1)
-        result = postal_service_calculator.compute(@package)
+        result = subject.compute(@package)
         expect(result).to eq(25.0)
       end
 
       it 'gives 6 when total price is 60 and weight is less than 1kg' do
         create_our_package(weight: 0.5, price: 60.0, quantity: 1)
-        result = postal_service_calculator.compute(@package)
+        result = subject.compute(@package)
         expect(result).to eq(6.0)
       end
 
       it 'gives 16 when total price is 40 and weight is less than 1kg' do
         create_our_package(weight: 0.5, price: 40.0, quantity: 1)
-        result = postal_service_calculator.compute(@package)
+        result = subject.compute(@package)
         expect(result).to eq(16.0)
       end
 
       it 'gives 30 when total price is 200 and weight is 25kg (split into two)' do
         create_our_package(weight: 25.0, price: 200.0, quantity: 1)
-        postal_service_calculator.preferred_max_price = 250
-        result = postal_service_calculator.compute(@package)
+        subject.preferred_max_price = 250
+        result = subject.compute(@package)
         expect(result).to eq(30.0)
       end
 
       it 'gives 12 when total price is 100, there are three items and their weight is unknown' do
-        order = create(:order)
-        [30.0, 40.0, 30.0].each do |price|
-          create(:line_item,
-            price: price,
-            quantity: 1,
-            order: order,
-            variant: create(:base_variant, weight: nil))
-        end
+        order   = create(:order)
+        variant = create(:base_variant, weight: nil)
+        [30.0, 40.0, 30.0].each { |price| create_line_item(order, variant, price: price) }
         order.line_items.reload
-        shipment = create(:shipment, order: order)
-        package = shipment.to_package
-
-        result = postal_service_calculator.compute(shipment)
+        package = create(:shipment, order: order)
+        result  = subject.compute(package)
         expect(result).to eq(12.0)
       end
 
       it 'gives 0 when total price is more than the MAX, for any number of items' do
         create_our_package(weight: 25.0, price: 350.0, quantity: 1)
-        postal_service_calculator.preferred_max_price = 300
-        result = postal_service_calculator.compute(@package)
+        subject.preferred_max_price = 300
+        result = subject.compute(@package)
         expect(result).to eq(0.0)
       end
     end
   end
 
   describe 'when preferred max weight, length and width are 18 kg, 120 cm and 60 cm' do
-    context '.available?' do
+    context '.available?(package)' do
       it 'is false when item weighs more than 18kg' do
         create_our_package(weight: 20, height: 70, width: 30, depth: 30)
-        expect(postal_service_calculator.available?(@package)).to be(false)
+        expect(subject.available?(@package)).to be(false)
       end
 
       it 'is false when item is longer than 120cm' do
         create_our_package(weight: 10, height: 130, width: 30, depth: 30)
-        expect(postal_service_calculator.available?(@package)).to be(false)
+        expect(subject.available?(@package)).to be(false)
       end
 
       it 'is false when item is wider than 60cm' do
         create_our_package(weight: 10, height: 80, width: 70, depth: 30)
-        expect(postal_service_calculator.available?(@package)).to be(false)
+        expect(subject.available?(@package)).to be(false)
       end
-      end
+    end
 
-      context '.item_oversized?' do
+    context '.item_oversized?(variant)' do
       it 'is true if the longest side is more than 120cm' do
         create_our_package(weight: 10, height: 130, width: 40, depth: 30)
-        expect(postal_service_calculator.item_oversized?(@variant)).to be(true)
+        expect(subject.item_oversized?(@variant)).to be(true)
       end
 
       it 'is true if the second longest side is more than 60cm' do
         create_our_package(weight: 10, height: 80, width: 70, depth: 30)
-        expect(postal_service_calculator.item_oversized?(@variant)).to be(true)
+        expect(subject.item_oversized?(@variant)).to be(true)
       end
     end
   end
 
-  def create_our_package(args={})
-    params = {}
-    params.merge!(weight: args[:weight]) if args[:weight]
-    params.merge!(height: args[:height]) if args[:height]
-    params.merge!(width:  args[:width])  if args[:width]
-    params.merge!(depth:  args[:depth])  if args[:depth]
-    @variant = create(:base_variant, params)
+  def create_our_package(args = {})
+    @variant = create :base_variant, args.except!(:quantity)
+    order = create :order
+    create_line_item(order, @variant, args)
+    order.line_items.reload
+    @package = create :shipment, order: order
+  end
 
-    params = { variant: @variant }
-    params.merge!(price: args[:price])       if args[:price]
-    params.merge!(quantity: args[:quantity]) if args[:quantity]
-    @line_item = create(:line_item, params)
-
-    @order = @line_item.order
-    @order.line_items.reload
-    @shipment = create(:shipment, order: @order)
-    @package = @shipment.to_package
+  def create_line_item(order, variant, args = {})
+    create(
+      :line_item,
+      price:    args[:price] || BigDecimal.new('10.00'),
+      quantity: args[:quantity] || 1,
+      order:    order,
+      variant:  variant
+    )
   end
 end
