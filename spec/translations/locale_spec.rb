1440bd3d433611e86fc8799064f960f836ce290a
Tobias Bohwalli <hi@futhr.io>
2014-08-31 20:35:02 +0200

    Update spec syntax.

--- spec/translations/locale_spec.rb
+++ spec/translations/locale_spec.rb
@@ -1,13 +1,14 @@
-describe 'locale:' do
+RSpec.describe 'locale:' do
+
   Dir.glob('config/locales/*.yml') do |locale_file|
     context locale_file do
-      it { should be_parseable }
-      it { should have_valid_pluralization_keys }
-      it { should_not have_missing_pluralization_keys }
-      it { should have_one_top_level_namespace }
-      it { should_not have_legacy_interpolations }
-      it { should have_a_valid_locale }
-      it { should be_a_complete_translation_of 'config/locales/en.yml' }
+      it { is_expected.to be_parseable }
+      it { is_expected.to have_valid_pluralization_keys }
+      it { is_expected.not_to have_missing_pluralization_keys }
+      it { is_expected.to have_one_top_level_namespace }
+      it { is_expected.not_to have_legacy_interpolations }
+      it { is_expected.to have_a_valid_locale }
+      it { is_expected.to be_a_complete_translation_of 'config/locales/en.yml' }
     end
   end
 end
