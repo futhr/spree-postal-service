b73eab065dabbe2ab57d4c945a05ad4cb232df4f
Tobias Bohwalli <hi@futhr.io>
2017-02-03 14:26:19 +0100

    Fixes cops warnings.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ end
 
 ENV['RAILS_ENV'] ||= 'test'
 
-require File.expand_path('../dummy/config/environment.rb',  __FILE__)
+require File.expand_path('../dummy/config/environment.rb', __FILE__)
 
 require 'pry'
 require 'ffaker'
