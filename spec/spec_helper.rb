1adaaf0bb19389637afeb7a9f848564dc41786f3
Tobias Bohwalli <hi@futhr.io>
2026-08-16 09:03:06 +0100

    test: replace the legacy Spree harness
    Adopt solidus_dev_support and focused policy and adapter specs for the supported Ruby and Solidus runtime.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,39 +1,5 @@
-require 'simplecov'
-require 'coveralls'
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
-SimpleCov.start do
-  add_filter '/.bundle'
-  add_filter '/spec/'
-  add_filter '/lib/spree_postal_service/engine'
-  add_group 'Libraries', 'lib'
-end
+# frozen_string_literal: true
 
-ENV['RAILS_ENV'] ||= 'test'
-
-require File.expand_path('../dummy/config/environment.rb', __FILE__)
-
-require 'pry'
-require 'ffaker'
-require 'rspec/rails'
-
-ActiveRecord::Migration.maintain_test_schema!
-ActiveRecord::Migration.check_pending!
-
-RSpec.configure do |config|
-  config.fail_fast = false
-  config.filter_run focus: true
-  config.run_all_when_everything_filtered = true
-
-  config.mock_with :rspec
-  config.raise_errors_for_deprecations!
-  config.infer_spec_type_from_file_location!
-
-  config.expect_with :rspec do |expectations|
-    expectations.syntax = :expect
-  end
-end
-
-Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |file| require file }
+require "solidus_dev_support/rspec/coverage"
+require "solidus_dev_support/rspec/rails_helper"
+require "spree_postal_service"
---
+++ spec/spree/calculator/shipping/postal_service_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+RSpec.describe Spree::Calculator::Shipping::PostalService do
+  subject(:calculator) { described_class.new }
+
+  let(:variant) do
+    instance_double(
+      Spree::Variant,
+      weight: BigDecimal("10"),
+      width: BigDecimal("30"),
+      depth: BigDecimal("30"),
+      height: BigDecimal("30")
+    )
+  end
+
+  let(:content_item) do
+    instance_double(
+      Spree::Stock::ContentItem,
+      variant: variant,
+      quantity: 1,
+      price: BigDecimal("100")
+    )
+  end
+
+  let(:order) { instance_double(Spree::Order, item_total: BigDecimal("100")) }
+  let(:package) { instance_double(Spree::Stock::Package, contents: [content_item], order: order) }
+
+  it "uses the current Solidus compute_package API" do
+    expect(calculator.compute_package(package)).to eq(BigDecimal("15"))
+  end
+
+  it "uses package contents for parcel constraints" do
+    allow(variant).to receive(:weight).and_return(BigDecimal("20"))
+    expect(calculator.available?(package)).to be(false)
+  end
+
+  it "returns unavailable for invalid configuration instead of crashing rate selection" do
+    calculator.preferred_weight_table = "2 1"
+    expect(calculator.available?(package)).to be(false)
+  end
+
+  it "surfaces invalid configuration through calculator validation" do
+    calculator.preferred_weight_table = "2 1"
+    expect(calculator).not_to be_valid
+    expect(calculator.errors[:base]).not_to be_empty
+  end
+end
